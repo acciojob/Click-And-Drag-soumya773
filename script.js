30b89208b25a27d6065f9108db2dd69cd7da0631
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-05-12 12:23:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,25 +5,22 @@ let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
+  startX = e.pageX;
   scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
 });
 
 slider.addEventListener('mouseup', () => {
   isDown = false;
-  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
 });
 
 slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
   e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 1; // adjust speed if needed
+  const x = e.pageX;
+  const walk = x - startX;
   slider.scrollLeft = scrollLeft - walk;
 });
